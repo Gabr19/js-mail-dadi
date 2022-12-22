f5d1ae59efce875cdc97ee0ecf94f5d592387a95
Gabr19 <sannagabriele19@gmail.com>
2022-12-22 18:57:10 +0100

    aggiunta let

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 const user = prompt ('inserisci la tua email:');
 console.log(user)
 
-const iscritti = ["1@mail.com" , "2@mail.com" , "3@mail.com"]
+const iscritti = ["1@mail.com"]
 console.log('iscritti' , iscritti , typeof iscritti);
 
-if (user == iscritti) {
+let x = user;
+let y = iscritti;
+
+if (x == y) {
     alert ("accesso consentito")
+    
+    
 }
 
 else {
     alert("accesso negato")
 }
+
