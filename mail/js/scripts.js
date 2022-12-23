44cfe1fd30f3bcb538daec672c038f2cde68c15c
Gabr19 <sannagabriele19@gmail.com>
2022-12-23 11:44:10 +0100

    .

--- mail/js/scripts.js
+++ mail/js/scripts.js
@@ -1,19 +1,33 @@
 const user = prompt ('inserisci la tua email:');
-console.log(user)
+console.log('user' , user, typeof user)
 
-const iscritti = ["1@mail.com" , "2@mail.com" , "3@mail.com" ]
+const iscritti = ['1@mail.com' , '2@mail.com' , '3@mail.com'];
 console.log('iscritti' , iscritti , typeof iscritti);
 
-let x = user;
-let y = iscritti.value;
+let validEmail = true;
 
-if (x == y) {
-    alert ("accesso consentito")
-    
-    
+if (validEmail == true) {
+
+    let canAccess = false;
+
+    for(let i = 0; i <= iscritti.lenght; i = i + 1){
+
+    console.log('iscritti[i]', iscritti[i], typeof iscritti[i])
+        if(iscritti [i] == user){
+            canAccess = true;
+            
+    if(canAccess == true){
+        alert('puoi entrare')
+    }
+    else{
+        alert('non puoi entrare')
+    }
+        
+    }
+}
 }
 
 else {
-    alert("accesso negato")
+        alert("accesso negato")
 }
 
